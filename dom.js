a70721c727f5a96ae1942239f1fe1b08708ea6ec
Deepak Kumar <deepakgzsccet@hotmail.com>
2023-02-23 02:11:41 +0530

    Task5Final

--- dom.js
+++ dom.js
@@ -1,9 +1,18 @@
 
-var listGroup= document.getElementsByClassName('list-group-item');
-console.log(listGroup);
-listGroup[2].style.backgroundColor='green';
-for(var i=0; i<listGroup.length; i++){
-    listGroup[i].style.fontWeight= 'bold';
+// var listGroup= document.getElementsByClassName('list-group-item');
+// console.log(listGroup);
+// listGroup[2].style.backgroundColor='green';
+// for(var i=0; i<listGroup.length; i++){
+//     listGroup[i].style.fontWeight= 'bold';
+// }
+
+var li= document.getElementsByTagName('li');
+console.log(li);
+li[2].style.backgroundColor='green';
+for(var i=0; i<li.length; i++){
+    li[i].style.fontWeight= 'bold';
 }
+
+   
 
    
